cff65f779672a92d7738978f750962595bd470f4
Sennan Li <sennanli@Mac.lan>
2024-10-11 22:47:11 +0200

    updated the project links

--- src/homepage/constants/constants.js
+++ src/homepage/constants/constants.js
@@ -1,33 +1,33 @@
 export const articleTitle8 = 'Python: Weather Forecast App'
 export const articleDate8 = '2024'
-export const articleLink8 = 'weather-forecast-sennanli.streamlit.app'
+export const articleLink8 = 'https://weather-forecast-sennanli.streamlit.app'
 
 
 export const articleTitle7 = 'Python: Todo App'
 export const articleDate7 = '2024'
-export const articleLink7 = 'todo-sennanli.streamlit.app'
+export const articleLink7 = 'https://todo-sennanli.streamlit.app'
 
 export const articleTitle6 = 'React Native: Home Care Mobile App'
 export const articleDate6 = '2023'
-export const articleLink6 = 'sennanli.com/news'
+export const articleLink6 = 'https://sennanli.com/news'
 
 export const articleTitle5 = 'React: To Do Web App'
 export const articleDate5 = '2023'
-export const articleLink5 = 'todo.sennanli.com'
+export const articleLink5 = 'https://todo.sennanli.com'
 
 export const articleTitle4 = 'React: Notes Web App'
 export const articleDate4 = '2023'
-export const articleLink4 = 'notes.sennanli.com'
+export const articleLink4 = "https://notes.sennanli.com"
 
 export const articleTitle3 = 'Responsive Web: E-Commerce Website'
 export const articleDate3 = '2023'
-export const articleLink3 = 'store.sennanli.com'
+export const articleLink3 = "https://store.sennanli.com"
 
 export const articleTitle2 = 'Responsive Web: notebook Website'
 export const articleDate2 = '2022'
-export const articleLink2 = 'notebook.sennanli.com'
+export const articleLink2 = 'https://notebook.sennanli.com'
 
 export const articleTitle1 = 'Responsive Web: Hometown Website'
 export const articleDate1 = '2021'
-export const articleLink1 = 'hometown.sennanli.com' 
+export const articleLink1 = 'https://hometown.sennanli.com' 
 
